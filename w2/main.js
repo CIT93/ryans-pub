61c4f7b7556b6cc76a20566016139af0e05a59c4
Ryan Stoltenberg <kakoa14@aol.com>
2025-01-31 02:52:52 -0800

    coding is done

---
+++ w2/main.js
@@ -0,0 +1,20 @@
+// 1. Count members of household.
+const myFamily = 10
+// 2. Size of home.
+const myHomesize = 10
+// 3. Food choices.
+const myFood = 8
+// 4. Water consumption.
+const myWater = 3
+// 5. Household purchases per year.
+const myPurchases = 2
+// 6. Waste produced.
+const myGarbage = 20
+// 7. Waste recycled.
+const myRecycle = 12
+// 8. Transportation score.
+const myCommute = 4
+// 9. Total Points.
+const cfpTotal = document.querySelector("h2")
+// 10. Write JS to update the rendered html.
+cfpTotal.textContent = myFamily + myHomesize + myFood + myWater + myPurchases + myGarbage - myRecycle + myCommute
